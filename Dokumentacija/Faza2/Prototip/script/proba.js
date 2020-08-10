bd7afc092259ec5ea406f7dab17de155a10ed328
nristic1003 <61749935+nristic1003@users.noreply.github.com>
2020-08-10 23:29:51 +0200

    finalno

--- Dokumentacija/Faza2/Prototip/script/proba.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("nav");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
